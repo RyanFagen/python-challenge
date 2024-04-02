b396b00f5ba9c93b8e6a7e901db435bc9f1ee49d
Ryan Fagen <rfagen@stumail.jccc.edu>
2024-04-02 02:43:28 -0500

    added current_value variable to make the max/min process easier

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,6 +2,7 @@ import csv
 bank_file_path = "PyBank\Resources\money_data.csv"
 total_months = 0
 net_total = 0
+current_value = 0
 max_increase = 0
 min_increase = 0
 max_month = "null"
@@ -11,6 +12,7 @@ with open(bank_file_path) as bank_file:
     next(csv_file)
     for row in csv_file:
         total_months += 1
+        current_value = int(row[1])
         net_total += int(row[1])
 print(f'{total_months}')
 print(f'{net_total}')
