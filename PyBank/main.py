a43f7330a08dc611d555c013455dba2bf2008566
Ryan Fagen <rfagen@stumail.jccc.edu>
2024-04-02 02:23:05 -0500

    for loop testing; with statement broken

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,3 +9,6 @@ min_month = "null"
 with open(bank_file_path) as bank_file:
     csv_file = csv.reader(bank_file)
     next(csv_file)
+    for row in csv_file:
+        total_months += 1
+print(f'{total_months}')
